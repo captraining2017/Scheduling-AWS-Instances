47a7c434049ae57bbd0b42755efa47d242ae503e
captraining2017 <captrainingfeb2017@gmail.com>
2017-04-04 15:29:16 +0530

    Add files via upload

---
+++ Scheduler_stop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -A exception_list
+declare -A temp_list
+
+# User input values
+env_type_value=$1
+region=$2
+exception_list=$3
+
+# Validating the user inputs
+if [ "$env_type_value" != "" ] && [ "$region" != "" ]
+then
+# Describing instances with filters to fetch instance-id list
+temp_list=$(eval "aws ec2 describe-instances --region $region --filters "Name=tag:Environment,Values=$env_type_value" --query Reservations[].Instances[].InstanceId --output text")
+
+# Validating instance list
+if [ "$temp_list" != "" ]
+then
+
+temp01=$(echo ${temp_list[@]} ${exception_list[@]} | tr ' ' '\n' | sort | uniq -u | tr '\n' ' ')
+
+# Stopping all the instances in the list
+stopping_instance=$(aws ec2 stop-instances --region $region --instance-ids $temp01)
+echo "Following instances are stopped in the $region region,"
+echo -e "\t$temp01"
+else
+echo "No Instance found. Please check the following tag values for $region region"
+echo -e "\tEnvironment: $env_type_value"
+fi
+else
+echo -e "Environment and region value will not be empty"
+fi
